59c05052c27c5c68b7171e3b42f887d49eb79765
Khoa Do <happyboy0412@gmail.com>
2023-02-22 15:44:17 +0700

    enable syntax highlight for astro ls

--- nvim/lua/kd/lsp/init.lua
+++ nvim/lua/kd/lsp/init.lua
@@ -2,21 +2,27 @@ require("kd.mason")
 
 local status_ok, lspconfig = pcall(require, "lspconfig")
 if not status_ok then
-	return
+  return
 end
 
 local handlers = require("kd.lsp.handlers")
 handlers.setup()
 
 local opts = {
-	on_attach = handlers.on_attach,
-	capabilities = handlers.capabilities,
+  on_attach = handlers.on_attach,
+  capabilities = handlers.capabilities,
 }
 
 local jsonls_opts = require("kd.lsp.settings.jsonls")
 lspconfig.jsonls.setup(vim.tbl_deep_extend("force", jsonls_opts, opts))
 
-lspconfig.astro.setup(opts)
+lspconfig.astro.setup(vim.tbl_deep_extend("force", {
+  init_options = {
+    typescript = {
+      serverPath = "~/.local/share/nvim/mason/packages/astro-language-server/node_modules/typescript/lib/tsserverlibrary.js"
+    }
+  }
+}, opts))
 lspconfig.clangd.setup(opts)
 lspconfig.cmake.setup(opts)
 lspconfig.cssls.setup(opts)
