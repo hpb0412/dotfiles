53a3e4c888095261180a117bac8a5daca5f0771c
Khoa Do <happyboy0412@gmail.com>
2023-04-08 16:48:18 +0700

    horizontal term

--- nvim/lua/plugins/toggleterm.lua
+++ nvim/lua/plugins/toggleterm.lua
@@ -16,7 +16,7 @@ return {
       start_in_insert = true,
       insert_mappings = true,
       persist_size = true,
-      direction = "float",
+      direction = "horizontal",
       close_on_exit = true,
       shell = vim.o.shell,
       float_opts = {
