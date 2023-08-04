4055c2bf88d87f97e9cd0ab1cc615a8fc1ddecec
Khoa Do <happyboy0412@gmail.com>
2023-08-04 18:05:46 +0700

    feat: remove filetype.nvim and p00f/nvim-ts-rainbow

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -7,12 +7,6 @@ return {
   "nvim-lua/plenary.nvim",           -- Useful lua functions used ny lots of plugins
   "nvim-tree/nvim-web-devicons",     -- font icons used in a lots of plugins
   "antoinemadec/FixCursorHold.nvim", -- needed to fix lsp doc highlight
-  {
-    "nathom/filetype.nvim",          -- better filetype => faster booting
-    init = function()
-      vim.g.did_load_filetypes = 1
-    end,
-  },
   "moll/vim-bbye",                -- Provide Bdelete & Bwipeout
 
   "L3MON4D3/LuaSnip",             -- snippet engine written in Lua
@@ -27,7 +21,6 @@ return {
   },
   "tamago324/nlsp-settings.nvim", -- language server settings defined in json
 
-  "p00f/nvim-ts-rainbow",         -- make each pair diffent colors (rainbow)
   "JoosepAlviste/nvim-ts-context-commentstring",
   "windwp/nvim-ts-autotag",       -- Autoclose tags
   'nvim-treesitter/nvim-tree-docs',
---
+++ nvim/lua/plugins/rainbow-delimiters.lua
@@ -0,0 +1,29 @@
+return {
+  "HiPhish/rainbow-delimiters.nvim",
+  init = function()
+    local status_ok, rainbow_delimiters = pcall(require, 'rainbow-delimiters')
+    if not status_ok then
+      return
+    end
+
+    vim.g.rainbow_delimiters = {
+      strategy = {
+        [''] = rainbow_delimiters.strategy['global'],
+        vim = rainbow_delimiters.strategy['local'],
+      },
+      query = {
+        [''] = 'rainbow-delimiters',
+        lua = 'rainbow-blocks',
+      },
+      highlight = {
+        'RainbowDelimiterRed',
+        'RainbowDelimiterYellow',
+        'RainbowDelimiterBlue',
+        'RainbowDelimiterOrange',
+        'RainbowDelimiterGreen',
+        'RainbowDelimiterViolet',
+        'RainbowDelimiterCyan',
+      },
+    }
+  end
+}
