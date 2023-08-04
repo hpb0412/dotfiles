7667823b6581e752c41b2b5636cd86f0e37123c2
Khoa Do <happyboy0412@gmail.com>
2023-08-04 21:47:27 +0700

    fix: astro lsp config to use ts server

--- nvim/lua/lsp/settings/astro.lua
+++ nvim/lua/lsp/settings/astro.lua
@@ -2,7 +2,9 @@ return {
   init_options = {
     typescript = {
       -- https://www.reddit.com/r/neovim/comments/10iixrz/comment/j706g5y/?utm_source=share&utm_medium=web2x&context=3
-      serverPath = vim.fs.normalize "~/.local/share/nvim/mason/packages/astro-language-server/node_modules/typescript/lib/tsserverlibrary.js"
+      -- according to this https://github.com/withastro/language-tools/issues/501
+      -- the key now `tsdk` not `serverPath` anymore, and it should be the directory path, not file path
+      tsdk = vim.fs.normalize "~/.local/share/nvim/mason/packages/astro-language-server/node_modules/typescript/lib"
     }
   }
 }
