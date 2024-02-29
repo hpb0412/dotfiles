34cf48b90150a56197d304cdd296b6071ba2c19f
Khoa Do <happyboy0412@gmail.com>
2024-02-29 13:55:36 +0700

    feat: integrate hurl tool

--- nvim/lua/autocmd.lua
+++ nvim/lua/autocmd.lua
@@ -18,6 +18,7 @@ vim.cmd [[
     au BufReadPost package.json set filetype=json
     au BufReadPost */php/*.conf set filetype=dosini
     au BufReadPost .env* set filetype=sh
+    au BufReadPost *.hurl set filetype=hurl
   augroup end
 ]]
 
