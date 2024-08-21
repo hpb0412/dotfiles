aeaf808fad5d46dc7def1396653550d51db41c47
Khoa Do <happyboy0412@gmail.com>
2024-08-21 09:20:24 +0700

    feat: update indentline

--- nvim-lazy/lua/plugins/indentline.lua
+++ nvim-lazy/lua/plugins/indentline.lua
@@ -24,13 +24,12 @@ return {
     ibl.setup({
       enabled = true,
       indent = {
-        char = "▏"
+        char = "⋮"
       },
       whitespace = {
         highlight = {
-          "CursorColumn",
+          -- "CursorColumn",
           "Whitespace",
-
         }
       },
       exclude = {
@@ -103,7 +102,7 @@ return {
     -- }
 
     -- HACK: work-around for https://github.com/lukas-reineke/indent-blankline.nvim/issues/59
-    vim.wo.colorcolumn = "99999"
+    -- vim.wo.colorcolumn = "99999"
 
     -- vim.cmd [[highlight IndentBlanklineIndent1 guifg=#E06C75 gui=nocombine]]
     -- vim.cmd [[highlight IndentBlanklineIndent2 guifg=#E5C07B gui=nocombine]]
@@ -111,7 +110,7 @@ return {
     -- vim.cmd [[highlight IndentBlanklineIndent4 guifg=#56B6C2 gui=nocombine]]
     -- vim.cmd [[highlight IndentBlanklineIndent5 guifg=#61AFEF gui=nocombine]]
     -- vim.cmd [[highlight IndentBlanklineIndent6 guifg=#C678DD gui=nocombine]]
-    vim.opt.list = true
+    -- vim.opt.list = true
     -- some useful icons: »«■¬
     vim.opt.listchars:append({
       -- space = '⋅',
